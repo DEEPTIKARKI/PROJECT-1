a68bc60dddace56ab49d70d4e7789cd62c9498b8
Deepti karki <deeptikarki122@gmail.com>
2023-02-18 19:06:44 +0530

    comitthechange

---
+++ Portfolio/sever.js
@@ -0,0 +1,76 @@
+// hello world in Nodejs
+console.log("deepti")
+
+// Values and Data type 
+
+// -primitive Data Types 
+
+//-- Number: 2, 3, 4, 4.5, 24
+//-- String: "Pw Skills" or 'Pw Skills'
+//-- Boolean: true, false
+//-- null
+//--undefined
+
+
+
+// -Non Primitive Data Type
+
+// Array:-
+
+// UNSMART METHOD TO DECLARE VARIABLE
+let name1 = "hitesh"
+let name2 = "sushant"
+let name3 = "rohit"
+    // SMART METHOD TO DECLARE VARIABLE
+let names = ["ritesh", "rohit", "kaif", 2, 3, false, ]
+
+
+
+
+// - Object: {}
+
+let myName = "Deepti"
+
+myName = "hitesh"
+
+let highScore = 300
+console.log('highScore')
+
+let pwSkillsCoursePrice = 3500
+console.log(pwSkillsCoursePrice);
+
+let pwCoursePrice = 200
+let gstOnCources = 36
+let finalPurchaseAmount = pwCoursePrice + gstOnCources
+console.log("Final amount to be paid:", finalPurchaseAmount);
+
+let pwDsaCourse = 500
+console.log("Final price of DSA Couse: ", pwDsaCourse + gstOnCources);
+
+// "%" Modules- It Define the reminder of the divide
+console.log("Answer is ", 5 % 2);
+console.log("Answer is", 5 / 2);
+console.log("Answer is:", 3 ** 3);
+
+let hiteshHighScore = 200
+let rajuHighScore = 300
+
+// comparison
+console.log(hiteshHighScore == rajuHighScore);
+
+
+let value1 = true
+let value2 = false
+let value3 = true
+let value4 = false
+
+console.log(value1 && value3);
+
+// and 
+let isLoggedIn = true
+let cardDetails = true
+
+// "||" --it means or
+let gmailAccount = true
+let emailAccount = false
+console.log(gmailAccount || emailAccount);
